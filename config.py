76d78521d39f1c477123988f6b1f78a1124361ae
Afza1112 <afzaaqddas@gmail.com>
2025-08-03 02:54:41 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,9 @@ from pymongo import MongoClient
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'change-this-in-production'
-    MONGO_URI = os.environ.get("MONGO_URI") or "mongodb://localhost:27017/"
-    DB_NAME = os.environ.get("DB_NAME") or "review_app"
-    MONGO_CLIENT = MongoClient(MONGO_URI)
+    # ...other configs...
+    SECRET_KEY = os.environ.get("SECRET_KEY", "supersecret")
+    MONGO_URI = os.environ.get("MONGO_URI")
+    DB_NAME = os.environ.get("DB_NAME", "review_app")
     UPLOAD_FOLDER = 'static/review_images'
+    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}  # <--- add this line
