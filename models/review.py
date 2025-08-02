a649763844e7eb4a8bb5d97591a1d2a63df4e3a2
Afza1112 <afzaaqddas@gmail.com>
2025-08-02 17:16:38 +0500

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,6 +1,6 @@
 from flask import current_app
 from pymongo import MongoClient
-
+import os
 def get_db():
     client = MongoClient(current_app.config['MONGO_URI'])
     db = client.get_default_database() or client["review_app"]
@@ -22,3 +22,14 @@ def approve_review_db(review_id):
     db = get_db()
     from bson.objectid import ObjectId
     db.reviews.update_one({"_id": ObjectId(review_id)}, {"$set": {"approved": True}})
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def ensure_upload_folder(app):
+    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+
+client = MongoClient(os.environ.get('MONGO_URI'))
+db = client["mongo"]           # <-- YAHAN "mongo" ya aapki DB ka naam ho
+reviews = db.reviews          # <-- YEH LINE HONI HI CHAHIYE
