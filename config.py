603e1a2658bbd6936829af62b0b74528ba4db0d0
Afza1112 <afzaaqddas@gmail.com>
2025-08-04 20:03:03 +0500

    Update config.py

--- config.py
+++ config.py
@@ -8,3 +8,7 @@ class Config:
     DB_NAME = os.environ.get("DB_NAME", "review_app")
     UPLOAD_FOLDER = 'static/review_images'
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}  # <--- add this line
+
+    SECRET_KEY = os.getenv("SECRET_KEY", "super-secret-key")
+    ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "paksa.admin")
+    ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD", "Paksa@385+")  # store hashed in real apps!
